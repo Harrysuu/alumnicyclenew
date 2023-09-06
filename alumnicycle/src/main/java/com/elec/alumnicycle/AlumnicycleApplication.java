31b6a59cc9db090cd78a2723508fec8c5ecf5e55
su-kaihang <harryskh@foxmail.com>
2023-09-06 11:10:26 +1000

    初始化项目

---
+++ alumnicycle/src/main/java/com/elec/alumnicycle/AlumnicycleApplication.java
@@ -0,0 +1,13 @@
+package com.elec.alumnicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnicycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumnicycleApplication.class, args);
+    }
+
+}
---
+++ alumnicycle/src/test/java/com/elec/alumnicycle/AlumnicycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elec.alumnicycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnicycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
