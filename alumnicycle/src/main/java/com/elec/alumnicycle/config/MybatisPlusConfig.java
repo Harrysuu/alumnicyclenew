acac7bb7bd0af1eb3f1c369fa6d56c50af4091e2
su-kaihang <harryskh@foxmail.com>
2023-10-07 17:11:20 +1100

    新增mybatisPlusConfig

---
+++ alumnicycle/src/main/java/com/elec/alumnicycle/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.elec.alumnicycle.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    //  不加@Bean注解无法生效
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
