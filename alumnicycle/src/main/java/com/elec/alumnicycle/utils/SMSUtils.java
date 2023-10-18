91f4e9587d8aff2b55aa7271560407c82a5a41ec
Shizitong Xu <shxu5142@uni.sydney.edu.au>
2023-10-18 22:41:05 +1100

    新增获取手机验证码

--- alumnicycle/src/main/java/com/elec/alumnicycle/utils/SMSUtils.java
+++ alumnicycle/src/main/java/com/elec/alumnicycle/utils/SMSUtils.java
@@ -5,7 +5,6 @@ import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 
 /**
@@ -14,26 +13,19 @@ import com.aliyuncs.profile.DefaultProfile;
 public class SMSUtils {
 
     /**
-     * 发送短信
-     * @param signName 签名
-     * @param templateCode 模板
-     * @param phoneNumbers 手机号
-     * @param param 参数
+     send message
      */
     public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-        DefaultProfile profile = DefaultProfile.getProfile("cn-shanghai", "LTAI5tEUuJSguxeFU93yA6uW", "pAojkyBW9kOQsJg7OXOXF54QFX7MCd");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tJBUj5MsHRe5wpudKXv", "xDpac8v5wOsTNh4mFkwyerE24XewCH");
         IAcsClient client = new DefaultAcsClient(profile);
-
         SendSmsRequest request = new SendSmsRequest();
-        request.setRegionId("cn-shanghai");
+        request.setRegionId("cn-hangzhou");
         request.setPhoneNumbers(phoneNumbers);
         request.setSignName(signName);
         request.setTemplateCode(templateCode);
-        request.setTemplateParam("{\"code\":\"1234\"}");
-        request.setMethod(MethodType.POST);//注意这四行，与官网所展示的方法不同
+        request.setTemplateParam("{\"code\": \"" + param + "\"}");
         try {
             SendSmsResponse response = client.getAcsResponse(request);
-            System.out.println("短信发送成功");
         }catch (ClientException e) {
             e.printStackTrace();
         }
