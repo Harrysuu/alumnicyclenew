69c49f2be26e00e6f9a64d6b00fcf4d097da0c32
su-kaihang <harryskh@foxmail.com>
2023-10-28 00:55:46 +1100

    删除shoppingCart页面注解

--- alumnicycle/src/main/java/com/elec/alumnicycle/controller/ShoppingCartController.java
+++ alumnicycle/src/main/java/com/elec/alumnicycle/controller/ShoppingCartController.java
@@ -1,13 +1,8 @@
 package com.elec.alumnicycle.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.elec.alumnicycle.common.AjaxRes;
-import com.elec.alumnicycle.common.BaseContext;
-import com.elec.alumnicycle.entity.SecondPost;
 import com.elec.alumnicycle.entity.ShoppingCart;
-import com.elec.alumnicycle.entity.params.SecondPostByIdParam;
-import com.elec.alumnicycle.entity.params.SecondPostParam;
 import com.elec.alumnicycle.entity.params.ShoppingCartByIdParam;
 import com.elec.alumnicycle.entity.params.ShoppingCartParam;
 import com.elec.alumnicycle.service.ShoppingCartService;
@@ -18,12 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
-import java.util.List;
 
-/**
- * 购物车
- */
 @Slf4j
 @RestController
 @RequestMapping("/shoppingCart")
@@ -44,41 +34,25 @@ public class ShoppingCartController {
     public AjaxRes<Page<ShoppingCart>> getPostByUserId(@RequestBody ShoppingCartByIdParam param){
         return shoppingCartService.getPostByUserId(param);
     }
-    /**
-     * 添加购物车
-     * @param shoppingCart
-     * @return
-     */
+
     @PostMapping("/add")
     @ApiOperation(value = "add item to the shopping cart")
     public AjaxRes<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart, HttpServletRequest request){
         return shoppingCartService.add(shoppingCart,request);
     }
 
-    /**
-     * 更新购物车
-     * @return
-     */
     @PostMapping("/update")
     @ApiOperation(value = "update shoppingCart")
     public AjaxRes<ShoppingCart> updateShoppingCart(@RequestBody ShoppingCart shoppingCart){
         return shoppingCartService.updateShoppingCart(shoppingCart);
     }
 
-    /**
-     * 清空购物车
-     * @return
-     */
     @GetMapping("/clean")
     @ApiOperation(value = "delete all items in the shopping cart")
     public AjaxRes<String> clean(){
         return shoppingCartService.clean();
     }
 
-    /**
-     * 购物车删除单个 入参 goodsId 必传 userId（用户从threadLocal中获取时非必传，否则必传）
-     * @return
-     */
     @PostMapping("/deleteOne")
     @ApiOperation(value = "delete one goods")
     public AjaxRes<String> deleteOne(@RequestBody ShoppingCart shoppingCart,HttpServletRequest request){
